ddcff08a1c0c8644c2f8fb5a5af07251e7dd4f7e
Phil Ting <pting.me+gh@gmail.com>
2022-10-22 14:02:42 -0700

    added routing to components

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,44 @@
 import { FC } from 'react';
-import { Counter } from './features/counter-new';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { CreateList, EditList, Lists } from './features/list';
+import { MovieDetails, Movies } from './features/movie';
+import { NavBar } from './features/nav-bar';
+import { NavDrawer } from './features/nav-drawer';
+import { PersonDetails } from './features/person';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Movies />,
+  },
+  {
+    path: '/movie',
+    element: <MovieDetails />,
+  },
+  {
+    path: '/person',
+    element: <PersonDetails />,
+  },
+  {
+    path: '/list',
+    element: <Lists />,
+  },
+  {
+    path: '/list/create',
+    element: <CreateList />,
+  },
+  {
+    path: '/list/edit',
+    element: <EditList />,
+  },
+]);
 
 const App: FC = () => {
   return (
     <div>
-      <Counter />
+      <NavBar />
+      <NavDrawer />
+      <RouterProvider router={router} />
     </div>
   );
 };
