40fb4dee0c184c050a24af4d7f09e9e088b27c78
Phil Ting <pting.me+gh@gmail.com>
2022-10-21 20:06:42 -0700

    added tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <div className="App">
+    <div className="">
       <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src="/vite.svg" className="logo" alt="Vite logo" />
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1 className="text-red-800">Vite + React</h1>
       <div className="card">
         <Counter />
         <button onClick={() => setCount((count) => count + 1)}>
--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
 import App from './App';
 import { store } from './app/store';
-import './index.css';
+import './main.css';
 
 const container = document.getElementById('root') as HTMLElement;
 const root = createRoot(container);
